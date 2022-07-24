062389298c69b3c560216549a7c6c1fb2caaed5c
gonghaojing <1344081134@qq.com>
2022-07-24 15:49:07 +0800

    init file

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Author: gonghaojing 1344081134@qq.com
+ * @Date: 2022-07-21 15:23:43
+ * @LastEditors: gonghaojing 1344081134@qq.com
+ * @LastEditTime: 2022-07-24 13:03:23
+ * @FilePath: \renren-fast-vue\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <transition name="fade">
+    <router-view></router-view>
+  </transition>
+</template>
+
+<script>
+  export default {
+  }
+</script>
---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+<script>
+export default {
+  name: 'category',
+  data () {
+    return {
+      menus: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  methods: {
+    handleNodeClick (data) {
+      console.log(data)
+    },
+    getMenu () {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then(({data}) => {
+        this.menus = data.data
+      })
+    }
+  },
+  created () {
+    this.getMenu()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
